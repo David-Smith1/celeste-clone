7df5ca9f544cac7114b51ace319f1f80bedb1190
David Smith <smitty6000@gmail.com>
2021-01-10 11:49:54 +0900

    hehyouagain guy fight trigger and bullet

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public GameObject blake, hehYouAgain, bulletPrefab;
+ 
+    Transform triggerFight;
+    public Transform firePoint;
+
+   
+
+    public static bool fightStart;
+
+
+
+    public float timer;
+    public float shootTimer;
+    public float waitingTime = 3f;
+    public float jumpForce = 5f;
+    public float counter = 0f;
+
+    public float bulletForce = 5f;
+
+    private Rigidbody2D enemyRb;
+   
+    private Vector2 positionDisplacement;
+    private Vector2 positionOrigin;
+    private float _timePassed;
+
+    public bool fired;
+
+    public bool canJump;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         enemyRb = hehYouAgain.GetComponent<Rigidbody2D>();
+
+       
+
+        float randomDistance = Random.Range(-3f, 3f);
+        positionDisplacement = new Vector2(randomDistance, 0);
+        positionOrigin = hehYouAgain.transform.position;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    
+        if (fightStart)
+        {
+            timer += Time.deltaTime;
+            shootTimer += Time.deltaTime;
+
+            if (timer > 2.5)
+            {
+                canJump = true;
+            }
+       
+
+
+            //enemyjump
+            if (timer > waitingTime && counter <= 4 && canJump)
+            {
+                enemyRb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+                timer = 0;
+                counter += 1;
+            }
+            //enemywalk
+            if (!canJump)
+            {
+                _timePassed += Time.deltaTime;
+                hehYouAgain.transform.position = Vector3.Lerp(positionOrigin, positionOrigin + positionDisplacement,
+                Mathf.PingPong(_timePassed, 2));
+            }
+
+            if (enemyRb.velocity.y == 0)
+            {
+                canJump = false;
+            }
+
+            //enemy attack
+            if (shootTimer > 3)
+            {
+                Fire();
+                shootTimer = 0;
+            }
+
+            //enemysuper attack
+            if (timer > waitingTime && counter > 4)
+            {
+                //FireBig();
+                counter = 0;
+                timer = 0;
+            }
+
+
+        }
+
+        
+
+      
+
+
+
+   
+       
+
+    }
+
+    
+    private void Fire()
+    {
+      Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        fired = true;
+        bulletPrefab.GetComponent<Rigidbody2D>().AddForce(-transform.right * bulletForce, ForceMode2D.Impulse);
+    }
+    
+    //private void FireBig()
+    //{
+      //GameObject BIGASSBULLET = Instantiate(projectile, transform.position, transform.rotation);
+      //BIGASSBULLET.GetComponent<Rigidbody2D>().AddForce(-transform.right * (bulletForce + 1), ForceMode2D.Impulse);
+    //}    
+
+}
---
+++ Assets/Scripts/animation.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animation : MonoBehaviour
+{
+
+    private Animator anim;
+    private movement move;
+    private Collision coll;
+    
+    public SpriteRenderer sr;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        coll = GetComponentInParent<Collision>();
+        move = GetComponentInParent<movement>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        anim.SetBool("onGround", coll.onGround);
+        anim.SetBool("onWall", coll.onWall);
+        anim.SetBool("onRightWall", coll.onRightWall);
+        anim.SetBool("wallGrab", move.wallGrab);
+        anim.SetBool("wallSlide", move.wallSlide);
+        anim.SetBool("usedSuperJump", move.usedSuperJump);
+        anim.SetBool("attack1", move.attack1);
+        anim.SetBool("attack2", move.attack2);
+        anim.SetBool("slide", move.slide);
+
+    }
+
+    public void SetHorizontalMovement(float x, float y, float yVel)
+    {
+        anim.SetFloat("HorizontalAxis", Mathf.Abs(x));
+        anim.SetFloat("VerticalAxis", y);
+        anim.SetFloat("VerticalVelocity", yVel);
+    }
+
+    public void SetTrigger(string trigger)
+    {
+        anim.SetTrigger(trigger);
+    }
+
+    public void ResetTrigger(string trigger)
+    {
+        anim.ResetTrigger(trigger);
+    }
+
+    public void Flip(int side)
+    {
+
+        if (move.wallSlide)
+        {
+            if (side == -1 && sr.flipX)
+                return;
+
+            if (side == 1 && !sr.flipX)
+            {
+                return;
+            }
+        }
+        
+        if (move.wallGrab)
+        {
+            if (coll.onLeftWall)
+            {
+                side = -1;
+            }
+
+            if (coll.onRightWall)
+            {
+                side = 1;
+            }
+        }
+
+        bool state = (side == 1) ? false : true;
+        sr.flipX = state;
+    }
+}
