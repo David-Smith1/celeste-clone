df4d85349b7cda1107f65971a91d6c4db0410f82
David Smith <smitty6000@gmail.com>
2021-01-11 15:44:55 +0900

    cutler store triggers

---
+++ Assets/CutlerdoorinShop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutlerdoorinShop : MonoBehaviour
+{
+    public bool nearCutlerDoor;
+    // Start is called before the first frame update
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.name.Equals("blake"))
+        {
+            CutlershopController.nearCutlerDoor = true;
+        }
+
+    }
+
+    // Update is called once per frame
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (coll.gameObject.name.Equals("blake"))
+        {
+            CutlershopController.nearCutlerDoor = false;
+        }
+
+    }
+}
---
+++ Assets/Scripts/CutlershopController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutlershopController : MonoBehaviour
+{
+    [SerializeField]
+    GameObject blake, trianglebutton;
+
+    public static bool nearCutlerDoor;
+    public static bool talkCutler;
+
+    public bool visitedShop;
+
+
+    Vector3 pos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Controller.visitedShop = true;
+
+        Vector3 pos = transform.position;
+        trianglebutton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (nearCutlerDoor)
+        {
+            //display ENTER??
+            trianglebutton.SetActive(true);
+            pos = new Vector3(blake.transform.position.x - .06f, blake.transform.position.y + .6f, 0);
+            trianglebutton.transform.position = pos;
+
+            if (Input.GetButton("Fire4"))
+            {
+                blake.gameObject.SetActive(false);
+                trianglebutton.SetActive(false);
+                //code to change to cutler store scene
+               
+                SceneManager.LoadScene("OpeningScene");
+            }
+        }
+
+        if (!nearCutlerDoor)
+        {
+            trianglebutton.SetActive(false);
+        }
+
+        if (talkCutler)
+        {
+            trianglebutton.SetActive(true);
+            pos = new Vector3(blake.transform.position.x - .06f, blake.transform.position.y + .6f, 0);
+            trianglebutton.transform.position = pos;
+
+            if (Input.GetButton("Fire4"))
+            {
+                trianglebutton.SetActive(false);
+               
+              //function for cutler conversation
+            }
+        }
+
+    }
+
+
+}
