fcaeca577cb7562ac2eb441e6b1f4e0d17ae25b0
David Smith <smitty6000@gmail.com>
2021-03-20 23:05:02 +0900

    health file location

---
+++ Assets/Scripts/Opening/health.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class health : MonoBehaviour
+{
+
+    public GameObject blake;
+
+    public float blakehealth; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       blakehealth = 100f;
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.name == "bullet(Clone)")
+        {
+            blakehealth -= 25;
+        }
+        if (blakehealth <= 0)
+        {
+            Destroy(blake);
+        }
+  
+    }
+}
