9430f3284e35be9178bfabeaee40af54bf562a88
David Smith <smitty6000@gmail.com>
2021-04-05 13:10:28 +0900

    dialogue box script test

---
+++ Assets/Scripts/Opening/texter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class texter : MonoBehaviour
+{
+    public string[] sentences;
+    public int index;
+    public float typingSpeed;
+
+    void Start()
+    {
+        StartCoroutine(Type());
+    }
+    
+    IEnumerator Type()
+    {
+        foreach(char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+        }
+    }
+}
